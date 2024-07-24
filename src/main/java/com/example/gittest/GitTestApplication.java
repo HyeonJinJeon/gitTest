67e3b2853177401016062b80d2f1dad358c12cae
h_jinnny <nd3807@naver.com>
2024-07-24 14:29:08 +0900

    test

--- src/main/java/com/example/gittest/GitTestApplication.java
+++ src/main/java/com/example/gittest/GitTestApplication.java
@@ -1,7 +1,9 @@
 package com.example.gittest;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GitTestApplication {
@@ -9,5 +11,10 @@ public class GitTestApplication {
     public static void main(String[] args) {
         SpringApplication.run(GitTestApplication.class, args);
     }
-
+    @Bean
+    public CommandLineRunner run() {
+        return args -> {
+            System.out.println(11);
+        };
+    }
 }
